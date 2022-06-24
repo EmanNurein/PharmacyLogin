645663224735bcd4fc09f28d1fc323e44f2a4df4
EmanNurein <eamw96:gmail.com>
2022-06-24 09:47:27 +0200

    update

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+// const port = 5000;
+const cors = require('cors');
+app.use(cors());
+const MongoClient = mongo.MongoClient;
+const dotenv = require('dotenv');
+dotenv.config()
+let port = process.env.PORT || 8230;
+
+const mongoUrl = "mongodb+srv://eman:QT8LoVUWDt0uSVFu@cluster0.bvz0d.mongodb.net/janlogin?retryWrites=true&w=majority";
+
+
+const AuthController = require('./controller/authController');
+app.use('/api/auth', AuthController);
+
+app.listen(port,() => {
+    console.log(`Running on port ${port}`)
+})
