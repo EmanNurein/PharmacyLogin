cc09bc1b8de8c43ba0db436a1159f5e04ac948c1
EmanNurein <eamw96:gmail.com>
2022-06-28 11:29:26 +0200

    update

--- db.js
+++ db.js
@@ -2,5 +2,5 @@ var mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost:27017/janlogin')
 
-mongoose.connect("mongodb+srv://eman:zBpbhFss67XFjl83@cluster0.bvz0d.mongodb.net/emaapharmacy?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://eman1:583qAKFafvlHv6x9@cluster0.bvz0d.mongodb.net/emaapharmacy?retryWrites=true&w=majority");
 
